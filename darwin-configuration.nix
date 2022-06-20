df701a1c85871fc45e55ba0dd9d9857fcf64eeb7
Naoya Ono <ono.naoyaa@gmail.com>
2022-06-20 11:28:46 +0800

    initial commit

---
+++ darwin-configuration.nix
@@ -0,0 +1,83 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ <home-manager/nix-darwin> ];
+
+  users.users.onoya = {
+    name = "onoya";
+    home = "/Users/onoya";
+  };
+
+  home-manager.users.onoya = { pkgs, ... }: {
+    home.packages = [
+      pkgs.vim
+      pkgs.git
+      pkgs.mas
+    ];
+
+    programs.git = {
+      enable = true;
+      userEmail = "ono.naoyaa@gmail.com";
+      userName = "Naoya Ono";
+
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+
+    programs.zsh = {
+      enable = true;
+
+      oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = [
+          "git"
+        ];
+      };
+    };
+  };
+
+  homebrew = {
+    enable = true;
+
+    casks = [
+      # "brave-browser"
+      "notion"
+      "visual-studio-code"
+      "messenger"
+      "iterm2"
+      "alfred"
+      "lastpass"
+      "slack"
+    ];
+
+    masApps = {
+      Magnet = 441258766;
+    };
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ 
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  system.defaults.dock.autohide = true;
+}
