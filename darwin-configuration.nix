b6646acaf2cd6602177a61873d8b3f5fffee71d6
Naoya Ono <ono.naoyaa@gmail.com>
2024-07-16 15:48:47 +0800

    minor changes

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -29,6 +29,21 @@
       pkgs.nodePackages.pnpm
     ];
 
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through "home.file".
+    home.file = {
+      # # Building this configuration will create a copy of "dotfiles/screenrc" in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = ./dotfiles/screenrc;
+
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
+
     programs.direnv.enable = true;
     programs.direnv.enableZshIntegration = true;
 
@@ -129,7 +144,10 @@
   # nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  };
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
