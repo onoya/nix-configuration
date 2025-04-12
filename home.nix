a7c2a21f57f4b500094eef25862119bb05746838
Naoya Ono <ono.naoyaa@gmail.com>
2025-04-13 00:51:09 +0800

    Update tmux config

--- home.nix
+++ home.nix
@@ -86,18 +86,37 @@ in
       shortcut = "a";
       historyLimit = 100000;
       baseIndex = 1;
+      newSession = true;
 
       plugins = [
         {
           plugin = pkgs.tmuxPlugins.resurrect;
           extraConfig = ''
             set -g @resurrect-capture-pane-contents 'on'
-            set -g @resurrect-strategy-nvim 'session'
           '';
         }
+        pkgs.tmuxPlugins.continuum
+        pkgs.tmuxPlugins.better-mouse-mode
+        pkgs.tmuxPlugins.sensible
       ];
 
       extraConfig = ''
+        set-option -g default-shell ${pkgs.zsh}/bin/zsh
+        set-option -g default-command ${pkgs.zsh}/bin/zsh
+
+        unbind %
+        bind | split-window -h -c "#{pane_current_path}"
+
+        unbind '"'
+        bind - split-window -v -c "#{pane_current_path}"
+
+        bind -r j resize-pane -D 5
+        bind -r k resize-pane -U 5
+        bind -r l resize-pane -R 5
+        bind -r h resize-pane -L 5
+
+        bind -r m resize-pane -Z
+
         set -g mouse on
       '';
     };
