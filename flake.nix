c4c34927c6672a2b72e163486647ec5a14528c8b
Naoya Ono <ono.naoyaa@gmail.com>
2025-04-12 18:26:01 +0800

    24.11

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "onoya's Darwin System";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
 
-    darwin.url = "github:LnL7/nix-darwin";
+    darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
--- home.nix
+++ home.nix
@@ -6,7 +6,7 @@ let
 in
 {
   home = {
-    stateVersion = "24.05";
+    stateVersion = "24.11";
 
     packages = [
       pkgs.vim
