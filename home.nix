4d6367ec9bdabfbdb6e89c5d5f4cf68bfbeea145
Naoya Ono <ono.naoyaa@gmail.com>
2025-07-22 11:03:12 +0800

    auto start tmux

--- home.nix
+++ home.nix
@@ -106,6 +106,13 @@ in
           "git"
         ];
       };
+
+      initContent = ''
+        # Auto-start tmux session
+        if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+          exec tmux new-session -A -s main
+        fi
+      '';
     };
 
     tmux = {
